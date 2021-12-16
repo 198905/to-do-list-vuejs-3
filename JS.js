48ebf2dfb4e2d2c5bac2f842a83cda0669816455
Eric Minel <66842416+198905@users.noreply.github.com>
2021-12-16 17:00:56 +0100

    Add files via upload
    files

---
+++ JS.js
@@ -0,0 +1,20 @@
+Vue.createApp ({
+data(){
+    return{
+        header:'TO DO LIST',
+        toDo:[],
+        newValue:'',
+        done:false
+    }
+},
+methods: {
+    add:function() {
+        this.toDo.push({index:this.toDo.length+1,label:this.newValue});
+        this.newValue="";
+        },
+    pop:function(item) {
+        this.toDo=this.toDo.filter(i=>i !==item)
+        },
+    }
+})
+.mount('#app');
